4f5808687ef1de5242e1f16ef97bcd4f336c6171
popenta <alexandru.p9913@gmail.com>
2020-11-27 13:17:20 +0200

    Authentication and roles

--- src/main/java/com/mycompany/parkinglot/servlet/Logout.java
+++ src/main/java/com/mycompany/parkinglot/servlet/Logout.java
@@ -5,19 +5,9 @@
  */
 package com.mycompany.parkinglot.servlet;
 
-import com.mycompany.parkinglot.common.CarDetails;
-import com.mycompany.parkinglot.ejb.CarBean;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.security.DeclareRoles;
-import javax.inject.Inject;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.HttpConstraint;
-import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,17 +17,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Alex
  */
-@DeclareRoles({"AdminRole", "ClientRole"})
-@ServletSecurity(
-        value = @HttpConstraint(
-        rolesAllowed = {"AdminRole"}
-        ))
-
-@WebServlet(name = "Cars", urlPatterns = {"/Cars"})
-public class Cars extends HttpServlet {
-    
-    @Inject
-    private CarBean carBean;
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -56,10 +37,10 @@ public class Cars extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Cars</title>");            
+            out.println("<title>Servlet Logout</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Cars at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet Logout at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -74,22 +55,11 @@ public class Cars extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            //processRequest(request, response);
-            request.setAttribute("activePage", "Cars");
-            request.setAttribute("numberOfFreeParkingSpots", 10);
-            
-            List<CarDetails> cars = carBean.getAllCars();
-            request.setAttribute("cars", cars);
-            
-            request.getRequestDispatcher("/WEB-INF/pages/cars.jsp").forward(request, response);
-        } catch (Exception ex) {
-            Logger.getLogger(Cars.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        request.logout();
+        response.sendRedirect(request.getContextPath());
     }
 
     /**
@@ -103,17 +73,7 @@ public class Cars extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String[] carIdsAsString = request.getParameterValues("car_ids");
-        if(carIdsAsString != null){
-            List<Integer> carIds = new ArrayList<>();
-            for(String carIdAsString : carIdsAsString){
-                carIds.add(Integer.parseInt(carIdAsString));
-            }
-            carBean.deleteCarsById(carIds);
-        }
-        
-        response.sendRedirect(request.getContextPath() + "/Cars");
+        processRequest(request, response);
     }
 
     /**
--- src/main/webapp/WEB-INF/pages/menu.jsp
+++ src/main/webapp/WEB-INF/pages/menu.jsp
@@ -1,4 +1,5 @@
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <a class="navbar-brand" href="${pageContext.request.contextPath}">Parking Lot</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse"
@@ -9,12 +10,18 @@
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item ${activePage eq 'Cars' ? ' active' : ''}">
-        <a class="nav-link" href="${pageContext.request.contextPath}/Cars">Cars</a>
-      </li>
-      <li class="nav-item ${activePage eq 'Users' ? ' active' : ''}">
-        <a class="nav-link" href="${pageContext.request.contextPath}/Users">Users</a>
-      </li>
+        <c:if test="${pageContext.request.isUserInRole('AdminRole')}">
+            <li class="nav-item ${activePage eq 'Cars' ? ' active' : ''}">
+              <a class="nav-link" href="${pageContext.request.contextPath}/Cars">Cars</a>
+            </li>
+        </c:if>
+        
+        <c:if test="${pageContext.request.isUserInRole('ClientRole')}">
+            <li class="nav-item ${activePage eq 'Users' ? ' active' : ''}">
+              <a class="nav-link" href="${pageContext.request.contextPath}/Users">Users</a>
+            </li>
+        </c:if>
+            
         <li class="nav-item ${pageContext.request.requestURI eq '/ParkingLot/about.jsp' ? ' active' : ''}">
             <a class="nav-link" href="${pageContext.request.contextPath}/about.jsp">About<span class="sr-only">(current)</span></a>
         </li>
@@ -36,7 +43,14 @@
     </ul>
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-            <a class="nav-link" href="${pageContext.request.contextPath}/Login">Login</a>
+            <c:choose>
+                <c:when test="${pageContext.request.getRemoteUser() == null}">
+                    <a class="nav-link" href="${pageContext.request.contextPath}/Login">Login</a>
+                </c:when>
+                    <c:otherwise>
+                        <a class="nav-link" href="${pageContext.request.contextPath}/Logout">Logout</a>
+                    </c:otherwise>
+            </c:choose>
         </li>
     </ul>
   </div>
